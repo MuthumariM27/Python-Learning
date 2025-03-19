670f129afb2d45c1e14f7e41a8cd485215f45bf2
MuthumariM27 <muthumari27m@gmail.com>
2025-03-19 20:35:22 +0530

    Add files via upload

---
+++ tuple store.py
@@ -0,0 +1,8 @@
+userName = input("Enter your name: ")
+userAge = input("Enter your age: ")
+userGender = input("Enter your gender: ")
+userCity = input("Enter your city: ")
+userJapaneseLevel = input("Enter your Japanese level: ")
+
+userDetails = (userName, userAge, userGender, userCity, userJapaneseLevel)
+print(userDetails)
